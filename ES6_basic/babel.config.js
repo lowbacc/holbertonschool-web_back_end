ebc497016ea8f1e0b4c10a03d9669afe614be001
lowbacc <skalski.matthis@gmail.com>
2024-11-13 19:53:02 +0100

    Update Babel configuration to specify target environment for presets

--- ES6_basic/babel.config.js
+++ ES6_basic/babel.config.js
@@ -1,1 +1,12 @@
-export const presets = ['@babel/preset-env'];
+module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+    ],
+};
