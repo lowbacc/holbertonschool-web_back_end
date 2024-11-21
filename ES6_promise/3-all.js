7bafd2955296353aa7b1482df6b24cef76bc4efc
lowbacc <skalski.matthis@gmail.com>
2024-11-21 10:30:35 +0100

    Add handleProfileSignup function to manage user signup with Promise.all

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+/*eslint-disable*/
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+    return Promise.all([uploadPhoto(), createUser()])
+        .then((results) => {
+            const [photo, user] = results;
+            console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+        })
+        .catch(() => {
+            console.log('Signup system offline');
+        });
+}
+
+export default handleProfileSignup;
