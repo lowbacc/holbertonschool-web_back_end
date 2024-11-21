54aad1ecf320da6fe40bf3f6254412666c690be4
lowbacc <skalski.matthis@gmail.com>
2024-11-21 10:35:35 +0100

    Add guardrail function to handle errors and track execution flow in math operations

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+/*eslint-disable*/
+function guardrail(mathFunction) {
+    const queue = [];
+    try {
+      const result = mathFunction();
+      queue.push(result);
+    } catch (error) {
+      queue.push(`Error: ${error.message}`);
+    } finally {
+      queue.push('Guardrail was processed');
+    }
+    return queue;
+  }
+  
+  export default guardrail;
+  
