6192d55c89fe37488158ed910ea70464b7c4855b
lowbacc <skalski.matthis@gmail.com>
2024-12-04 22:05:00 +0100

    Add floor module and function to return the floor value of a float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module that takes a float n as argument and returns its floor value. """
+
+import math
+
+
+def floor(n: float) -> int:
+    """ Function that takes a float n as argument and returns its floor value. """
+
+    return math.floor(n)
