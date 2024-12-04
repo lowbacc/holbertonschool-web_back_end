30472e1b7189acbee382d132bfbb9f1384149f6b
lowbacc <skalski.matthis@gmail.com>
2024-12-04 22:07:46 +0100

    Refactor docstrings in floor module for improved readability

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-""" Module that takes a float n as argument and returns its floor value. """
+""" Module that takes a float n as argument
+and returns its floor value. """
 
 import math
 
 
 def floor(n: float) -> int:
-    """ Function that takes a float n as argument and returns its floor value. """
+    """ Function that takes a float n as argument
+    and returns its floor value. """
 
     return math.floor(n)
