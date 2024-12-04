d304aabc16249532c4743307c42e6a3c50772fbc
lowbacc <skalski.matthis@gmail.com>
2024-12-04 22:02:44 +0100

    Add concat module and function to concatenate two strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module that takes two strings str1 and str2 as arguments"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Function that takes two strings str1 and str2 as arguments"""
+
+    return str1 + str2
