50cc5999e2bc21eec383fc389b7e9a1eacb27ef6
lowbacc <skalski.matthis@gmail.com>
2024-12-04 22:31:27 +0100

    Refactor wait_n coroutine to use asyncio.gather for improved performance

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,9 +9,6 @@ from basic_async_syntax import wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Asynchronous coroutine that takes an integer n and an integer max_delay"""
 
-    delays = []
-    for _ in range(n):
-        delay = await wait_random(max_delay)
-        delays.append(delay)
-
-    return delays
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
