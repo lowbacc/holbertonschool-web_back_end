ab4e59caf69618ad2a760b02e33884f6748d6d75
lowbacc <skalski.matthis@gmail.com>
2024-12-04 22:00:41 +0100

    Add module and function docstrings to clarify add function usage

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
+""" Module that takes a float a and a float b as arguments
+and returns their sum as a float. """
+
+
 def add(a: float, b: float) -> float:
+    """ Function that takes a float a and a float b as arguments
+    and returns their sum as a float. """
+
     return a + b
