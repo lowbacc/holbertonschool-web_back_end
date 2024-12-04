b6ba7ae9593185b84e918d06f06a8e4912ee1a12
lowbacc <skalski.matthis@gmail.com>
2024-12-04 22:21:29 +0100

    Add element_length module to return lengths of sequences in an iterable

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+""" Module that takes an iterable of sequences as argument"""
+
+from typing import List, Tuple, Iterable, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ Function that takes an iterable of sequences as argument"""
+
+    return [(i, len(i)) for i in lst]
