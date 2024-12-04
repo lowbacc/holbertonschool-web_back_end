3a7a29ab11396442bc540207f44d00d6794724f3
lowbacc <skalski.matthis@gmail.com>
2024-12-04 22:35:54 +0100

    Add wait_random coroutine for generating random delays

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,7 +3,15 @@
 
 import asyncio
 from typing import List
-from basic_async_syntax import wait_random
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ Asynchronous coroutine that takes an integer max_delay"""
+
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
