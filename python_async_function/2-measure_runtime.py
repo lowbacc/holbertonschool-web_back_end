193d1d25524f24b1eb4f128d39bd2a75cda8691d
lowbacc <skalski.matthis@gmail.com>
2024-12-05 10:10:08 +0100

    Fix formatting in measure_time function for consistency

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -10,7 +10,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
     """ Function that measures the total execution time for wait_n"""
-    
+
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
     end = time.time()
