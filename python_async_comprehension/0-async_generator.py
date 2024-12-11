2e1426cc088d6d36bfdbb6ce3698dbc292e4165d
lowbacc <skalski.matthis@gmail.com>
2024-12-11 19:51:12 +0100

    Format async_generator function by removing unnecessary whitespace

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,9 +5,10 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """Async generator function"""
-    
+
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
