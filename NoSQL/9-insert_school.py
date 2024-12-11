21d190813d42bc09ce61ffb2118a4d0cbae111ff
lowbacc <skalski.matthis@gmail.com>
2024-12-11 20:40:47 +0100

    Add script to insert a new document in a MongoDB collection

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection based on kwargs
+    :param mongo_collection: pymongo collection object
+    :param kwargs: key-value pairs to insert as document
+    :return: the new _id of the inserted document
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
