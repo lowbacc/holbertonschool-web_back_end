e572e6dc2466158919562b4fec73a0985d1df04e
lowbacc <skalski.matthis@gmail.com>
2024-12-11 20:03:35 +0100

    Remove unnecessary blank line in index_range function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -7,7 +7,7 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> tuple:
     """Return a tuple of size two containing a start index
     and an end index """
-    
+
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
