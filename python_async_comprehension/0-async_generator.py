893ad1d95f1b2e51fcb304bb546292f00e5fd228
lowbacc <skalski.matthis@gmail.com>
2024-12-11 19:35:02 +0100

    Add async generator module with random value generation

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Async generator module"""
+
+import asyncio
+import random
+
+async def async_generator():
+    """Async generator function"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
