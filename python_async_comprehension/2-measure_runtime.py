02cbcf3bd7bb510c64ce412146f14e6da1036369
lowbacc <skalski.matthis@gmail.com>
2024-12-11 19:57:13 +0100

    Add type hints for async_generator, async_comprehension, and measure_runtime functions

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -4,19 +4,29 @@
 import asyncio
 import random
 import time
+from typing import Generator
+from typing import List
 
-async def async_generator():
+
+async def async_generator() -> Generator[float, None, None]:
     """Async generator function"""
+
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
 
-async def async_comprehension():
-    """Collects 10 random numbers using async comprehension over async_generator"""
+
+async def async_comprehension() -> list[float]:
+    """Collects 10 random numbers using async comprehension
+    over async_generator"""
+
     return [i async for i in async_generator()]
 
-async def measure_runtime():
-    """Measures the total runtime of executing async_comprehension four times in parallel"""
+
+async def measure_runtime() -> float:
+    """Measures the total runtime of executing
+    async_comprehension four times in parallel"""
+
     start_time = time.perf_counter()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_time = time.perf_counter()
